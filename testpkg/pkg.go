5d830cb6d944e50245d4e12009530e8024974e00
Elena Morozova <lelenanam@gmail.com>
2017-07-10 13:27:52 -0700

    Edit pkg 3
    Signed-off-by: Elena Morozova <lelenanam@gmail.com>

--- testpkg/pkg.go
+++ testpkg/pkg.go
@@ -1,5 +1,5 @@
 package testpkg
 
 func Function() string {
-	return "test 2"
+	return "test 3"
 }
--- testpkg/pkg_test.go
+++ testpkg/pkg_test.go
@@ -3,7 +3,7 @@ package testpkg
 import "testing"
 
 func TestFunction(t *testing.T) {
-	if Function() != "test 2" {
+	if Function() != "test 3" {
 		t.Fatal("test failed")
 	}
 }
