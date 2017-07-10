708a51a6d01140a0840201d74e1d016fd44c1b4a
Elena Morozova <lelenanam@gmail.com>
2017-07-10 13:24:20 -0700

    Edit pkg fail 101
    Signed-off-by: Elena Morozova <lelenanam@gmail.com>

--- testpkg/pkg.go
+++ testpkg/pkg.go
@@ -1,5 +1,5 @@
 package testpkg
 
 func Function() string {
-	return "test10"
+	return "test101"
 }
--- testpkg/pkg_test.go
+++ testpkg/pkg_test.go
@@ -3,7 +3,7 @@ package testpkg
 import "testing"
 
 func TestFunction(t *testing.T) {
-	if Function() != "test10" {
+	if Function() != "test101" {
 		t.Fatal("test failed")
 	}
 }
